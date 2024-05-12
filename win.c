4a860d88299438ec2ade83c8c9dfa8659e925778
Joshua Matthews <jmatth93@gmail.com>
2024-05-11 20:50:15 -0500

    calling appropriate free calls

--- win.c
+++ win.c
@@ -1,5 +1,6 @@
 #include "win.h"
 #include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_surface.h>
 
 void create_win(struct win *w) {
   w->window = SDL_CreateWindow("Editlite", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -11,6 +12,6 @@ void create_win(struct win *w) {
 }
 
 void free_win(struct win *w) {
-  SDL_free(w->window);
-  SDL_free(w->screen);
+  SDL_DestroyWindow(w->window);
+  SDL_FreeSurface(w->screen);
 }
