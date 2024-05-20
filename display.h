16e1d490fc96415b561c4f21acc732e26e61376b
Joshua Matthews <jmatth93@gmail.com>
2024-05-20 00:04:58 -0500

    saving off work towards loading in data

---
+++ display.h
@@ -0,0 +1,16 @@
+#ifndef EDITLITE_DISPLAY_H
+#define EDITLITE_DISPLAY_H
+
+#include "char.h"
+#include "page.h"
+
+struct display {
+  size_t cur_buf;
+  struct characters glyphs;
+  struct page_manager pages;
+};
+
+int init_display(struct display* d, const struct win *w);
+int page_render(struct display *page, struct win *w);
+
+#endif
