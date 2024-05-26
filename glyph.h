be89739fc60015f4790408bf7b8f1ee941eb3a93
Joshua Matthews <jmatth93@gmail.com>
2024-05-26 12:48:54 -0500

    renaming some things

--- glyph.h
+++ glyph.h
@@ -1,5 +1,5 @@
-#ifndef EDITLITE_CHAR_H
-#define EDITLITE_CHAR_H
+#ifndef EDITLITE_GLYPH_H
+#define EDITLITE_GLYPH_H
 
 #include <SDL2/SDL_ttf.h>
 #include "win.h"
@@ -7,7 +7,7 @@
 #define CHAR_START_RANGE 32
 #define CHAR_END_RANGE 127
 
-struct characters {
+struct glyphs {
   TTF_Font *font;
   SDL_Texture* glyphs[CHAR_END_RANGE - CHAR_START_RANGE];
   SDL_Color color;
@@ -16,8 +16,8 @@ struct characters {
   size_t max_width;
 };
 
-int init_char(struct characters *ch, const struct win *w, const char* ttf_file);
-SDL_Texture* get_glyph(struct characters *ch, const char c);
-void free_char(struct characters *);
+int init_char(struct glyphs *ch, const struct win *w, const char* ttf_file);
+SDL_Texture* get_glyph(struct glyphs *ch, const char c);
+void free_char(struct glyphs *);
 
 #endif
