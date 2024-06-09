65e7ac45c4303386f3606387bc0ecc48f01d124b
Joshua Matthews <jmatth93@gmail.com>
2024-06-09 17:12:53 -0500

    small changes; new font

--- config.c
+++ config.c
@@ -5,5 +5,5 @@ void init_config(struct config *c) {
   c->font_size = 14;
   c->cursor_color = (SDL_Color){0xAA,0xAA,0xAA,0x77};
   c->font_color = (SDL_Color){0xFF,0xFF,0xFF,0xFF};
-  c->font_file = "resources/SourceCodePro-Regular.ttf";
+  c->font_file = "resources/RobotoMono-Regular.ttf";
 }
