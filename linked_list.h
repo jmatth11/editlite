00b05ca579c556731f5d61f50ad8e66a59020c40
Joshua Matthews <jmatth93@gmail.com>
2024-06-09 21:51:13 -0500

    trying to get newlines and delete lines working

--- linked_list.h
+++ linked_list.h
@@ -21,6 +21,7 @@ struct linked_list* linked_list_get_pos(struct linked_list *ll, size_t pos);
 struct linked_list* linked_list_get_end(struct linked_list *ll);
 size_t linked_list_get_len(struct linked_list *ll);
 int linked_list_delete(struct linked_list *ll, size_t pos);
+int linked_list_delete_node(struct linked_list *ll);
 void linked_list_free(struct linked_list *ll);
 void linked_list_free_all(struct linked_list *ll);
 
