f04a1043e1fdccdee9344ad3835e03d60ef19a68
Joshua Matthews <jmatth93@gmail.com>
2024-06-25 01:14:03 -0500

    have commands working through plugin system using dynamic libraries

--- command_prompt.h
+++ command_prompt.h
@@ -2,11 +2,20 @@
 #define EDITLITE_COMMAND_PROMPT_H
 
 #include <stddef.h>
-#include "display.h"
-#include "win.h"
-#include "gap_buffer.h"
-#include "page.h"
+#include "plugin_interface.h"
+#include "deps/array_template/array_template.h"
 
-bool command_prompt_display(struct display *d, struct win *w);
+struct command {
+  void* handler;
+  char *shared_library;
+  bool (*action)(struct plugin_interface *d);
+  void (*get_display_prompt)(const char **out);
+};
+
+generate_array_template(command, struct command);
+
+bool command_init(struct command *cmd);
+bool command_load(struct command *cmd);
+bool command_unload(struct command *cmd);
 
 #endif
