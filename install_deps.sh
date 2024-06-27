e0bc04183e105d054359b1027166c31e4ea6d034
Joshua Matthews <joshua.matthews@patracorp.com>
2024-06-27 10:42:30 -0500

    small changes

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y libsdl2-dev libsdl2-ttf-dev
-git clone https://github.com/jmatth11/array_template.git deps
-git clone https://github.com/cktan/tomlc99.git deps
+git clone https://github.com/jmatth11/array_template.git deps/array_template
+git clone https://github.com/cktan/tomlc99.git deps/tomlc99
 
 cd deps/tomlc99/
 make
