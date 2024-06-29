8fb63a23400490a7cc6159581168aaeb6796df07
Joshua Matthews <jmatth93@gmail.com>
2024-06-29 12:16:39 -0500

    fixed scrolling issues with ctrl d/u

--- scrolling.c
+++ scrolling.c
@@ -7,12 +7,17 @@ void handle_row_scroll(struct display *d, struct display_dim dim) {
     fprintf(stderr, "could not get current page for handle_row_scroll.\n");
     return;
   }
-  const size_t c_row = d->cursor.pos.row;
   const size_t page_len = linked_list_get_len(cur_page->lines);
-  if (c_row >= page_len) {
+  if (d->cursor.pos.row < 0) {
+    d->cursor.pos.row = 0;
+  }
+  if (d->cursor.pos.row >= page_len) {
     d->cursor.pos.row = page_len - 1;
-  } else if (c_row >= cur_page->row_offset + dim.row) {
-    cur_page->row_offset++;
+  }
+  const size_t c_row = d->cursor.pos.row;
+  if (c_row >= cur_page->row_offset + dim.row) {
+    const int offset = c_row - (cur_page->row_offset + dim.row);
+    cur_page->row_offset += offset + 1;
   } else if (c_row < cur_page->row_offset) {
     cur_page->row_offset = c_row;
   }
@@ -30,6 +35,7 @@ void handle_col_scroll(struct display *d, struct display_dim dim) {
   }
   struct linked_list *cur_line = linked_list_get_pos(cur_page->lines, d->cursor.pos.row);
   const int gap_buffer_len = gap_buffer_get_len(&cur_line->value.chars);
+  if (d->cursor.pos.col < 0) d->cursor.pos.col = 0;
   if (d->cursor.pos.col >= gap_buffer_len) {
     char tmp = ' ';
     gap_buffer_get_char(&cur_line->value.chars, gap_buffer_len - 1, &tmp);
@@ -38,10 +44,13 @@ void handle_col_scroll(struct display *d, struct display_dim dim) {
     } else {
       d->cursor.pos.col = gap_buffer_len;
     }
-  } else if (d->cursor.pos.col >= cur_page->col_offset + dim.col) {
-    cur_page->col_offset++;
-  } else if (d->cursor.pos.col < cur_page->col_offset) {
-    cur_page->col_offset = d->cursor.pos.col;
+  }
+  const size_t c_col = d->cursor.pos.col;
+  if (c_col >= cur_page->col_offset + dim.col) {
+    const int offset = c_col - (cur_page->col_offset + dim.col);
+    cur_page->col_offset += offset + 1;
+  } else if (c_col < cur_page->col_offset) {
+    cur_page->col_offset = c_col;
   }
 }
 
