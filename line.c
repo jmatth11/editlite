abe118f86cdad75821f079f2d4fb42c378fa263f
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:53 -0600

    restructured and using linked list for line management. should allow for easier insertion/deletion of lines

---
+++ line.c
@@ -0,0 +1,15 @@
+#include "line.h"
+
+#define LINE_CHAR_BUFF_SIZE 10
+
+int init_line(struct line *l) {
+  if (!gap_buffer_init(&l->chars, LINE_CHAR_BUFF_SIZE)) return 1;
+  l->load_pos = 0;
+  l->start_pos = 0;
+  return 0;
+}
+
+void free_line(struct line *l) {
+  gap_buffer_free(&l->chars);
+}
+
