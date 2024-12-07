483148266ce9a6b135f7b1996a0d0243ce626e9b
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:52 -0600

    created ttf fonts and char buffer

---
+++ char.h
@@ -0,0 +1,18 @@
+#ifndef EDITLITE_CHAR_H
+#define EDITLITE_CHAR_H
+
+#include <SDL2/SDL_ttf.h>
+#include "win.h"
+
+struct characters {
+  TTF_Font *font;
+  SDL_Texture* glyphs[96];
+  SDL_Color color;
+  size_t size;
+};
+
+int init_char(struct characters *ch, const struct win *w, const char* ttf_file);
+SDL_Texture* get_glyph(struct characters *ch, const char c);
+void free_char(struct characters *);
+
+#endif
