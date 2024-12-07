ca4cc20ca0e699409dd051b3612f7867411fb473
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:54 -0600

    trying to flesh out find plugin

---
+++ plugins/find/find.c
@@ -0,0 +1,42 @@
+#include "find.h"
+#include <page.h>
+#include <deps/array_template/array_template.h>
+
+const char *prompt = "find files";
+struct find_info op;
+bool showMenu = false;
+
+bool setup(struct plugin_interface *pi) {
+  return true;
+}
+
+bool action(struct plugin_interface *pi) {
+  if (op.locs.location_data != NULL) {
+    free_location_array(&op.locs);
+  }
+  init_location_array(&op.locs, 1);
+  pi->dispatch(pi, DISPATCH_PLUGIN_TEXTINPUT, NULL);
+  showMenu = true;
+  return true;
+}
+
+bool render(struct display *d, struct display_dim *dim) {
+  if (showMenu) {
+    // TODO render find display
+  }
+  return true;
+}
+
+bool event(SDL_Event *e, struct display *d) {
+
+  return true;
+}
+
+void get_display_prompt(const char **out) {
+  *out = prompt;
+}
+
+bool cleanup(struct plugin_interface* pi) {
+  free_location_array(&op.locs);
+  return true;
+}
