d50311e0b25a9d57d2a047f3dbc70e66518c63fc
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:54 -0600

    needed to extend gab buffer implementation to include a way to easily get the string also include function pointer

--- gap_buffer.h
+++ gap_buffer.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
 #endif
 
 /**
@@ -16,6 +16,7 @@ struct gap_buffer {
   size_t len;
   size_t cursor_start;
   size_t cursor_end;
+  char* (*get_str)(struct gap_buffer*);
   char *buffer;
 };
 
@@ -80,9 +81,21 @@ bool gap_buffer_delete(struct gap_buffer *gb);
  */
 bool gap_buffer_delete_seq(struct gap_buffer *gb, size_t n);
 /**
+ * Get the string value of the gap buffer.
+ * User is responsible for freeing the returned string.
+ *
+ * @param gb The gap buffer.
+ * @returns The constructed string.
+ */
+char *gap_buffer_get_str(struct gap_buffer *gb);
+/**
  * Free the gap buffer internals.
  * @param gb The gap buffer.
  */
 void gap_buffer_free(struct gap_buffer *gb);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- plugins/find/search.c
+++ plugins/find/search.c
@@ -1,8 +1,18 @@
 #include "search.h"
 #include "page.h"
+#include <string.h>
 
 
 void search_word_options(struct display *d, struct find_info *op) {
   struct page *cur_page = &d->page_mgr.pages.page_data[d->cur_buf];
-  cur_page->lines
+  struct linked_list *cur_line = cur_page->lines;
+  while (cur_line != NULL) {
+    struct gap_buffer *gb = &cur_line->value.chars;
+    char *buf = gb->get_str(gb);
+    size_t buf_len = strlen(buf);
+    for (int i = 0; i<buf_len; ++i) {
+      // TODO finish string compare and search
+    }
+    free(buf);
+  }
 }
