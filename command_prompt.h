a3e46c4a72a6a6b24beee50fa8fb2bb0e49fda98
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:54 -0600

    added render function for plugins to tap into.

--- command_prompt.h
+++ command_prompt.h
@@ -1,9 +1,14 @@
 #ifndef EDITLITE_COMMAND_PROMPT_H
 #define EDITLITE_COMMAND_PROMPT_H
 
+#include <SDL2/SDL_render.h>
 #include <stddef.h>
 #include "plugin_interface.h"
 #include "deps/array_template/array_template.h"
+
+// forward declares
+struct display;
+struct display_dim;
 
 struct command {
   void* handler;
@@ -11,6 +16,7 @@ struct command {
   bool (*setup)(struct plugin_interface *);
   bool (*action)(struct plugin_interface *);
   void (*get_display_prompt)(const char **out);
+  bool (*render)(SDL_Renderer *, struct display *, struct display_dim *);
   bool (*cleanup)(struct plugin_interface *);
 };
 
