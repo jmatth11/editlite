de2c5e3e570c4f72b9bf2e0cd3b950debd139933
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:53 -0600

    have file traversal working

--- plugins/files/files.c
+++ plugins/files/files.c
@@ -3,33 +3,45 @@
 #include <display.h>
 #include <menu.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
 #include <dirent.h>
 #include <sys/stat.h>
 
+struct dir_info {
+  char *base;
+  char *cur_dir;
+  char *name;
+  DIR *dir;
+};
+
 const char *prompt = "view files";
 menu_item_array buffer;
-char *cwd = NULL;
-DIR *dir = NULL;
-
+struct dir_info dir_t = {
+  .base = NULL,
+  .cur_dir = NULL,
+  .name = NULL,
+  .dir = NULL,
+};
 bool gen_items_from_dir(const char *wd, menu_item_array *buf);
 
 bool setup(struct plugin_interface* pi) {
   size_t size = 1024;
-  cwd = malloc(sizeof(char) *size);
-  char *ptr = getcwd(cwd, size);
+  dir_t.base = malloc(sizeof(char) *size);
+  char *ptr = getcwd(dir_t.base, size);
   for (; ptr == NULL; size *= 2) {
-    if ((cwd = realloc(cwd, size)) == NULL) {
+    if ((dir_t.base = realloc(dir_t.base, size)) == NULL) {
       fprintf(stderr, "allocation error\n.");
       return false;
     }
-    ptr = getcwd(cwd, size);
+    ptr = getcwd(dir_t.base, size);
     if (ptr == NULL & errno != ERANGE) {
       fprintf(stderr, "getting current working directory failed.\n");
       return false;
     }
   }
+  dir_t.cur_dir = dir_t.base;
   return true;
 }
 
@@ -42,15 +54,23 @@ bool is_dir(const char *path) {
 
 bool file_selected(struct display *d, void *ctx) {
   char *filename = (char*)ctx;
+  size_t cur_dir_len = strlen(dir_t.cur_dir);
+  char *full_path = malloc(sizeof(char) * (cur_dir_len + strlen(filename) + 1));
+  sprintf(full_path, "%s%c%s", dir_t.cur_dir, '/', filename);
   // TODO handle more than top level directory
-  if (is_dir(filename)) {
-    if (!gen_items_from_dir(filename, &buffer)) {
+  if (is_dir(full_path)) {
+    // free previous path if it was not the base path
+    if (dir_t.base != dir_t.cur_dir) {
+      free(dir_t.cur_dir);
+    }
+    dir_t.cur_dir = full_path;
+    if (!gen_items_from_dir(full_path, &buffer)) {
       return false;
     }
     d->pi.dispatch(&d->pi, DISPATCH_MENU, &buffer);
     free_menu_item_array(&buffer);
   } else {
-    d->pi.dispatch(&d->pi, DISPATCH_NEW_PAGE, filename);
+    d->pi.dispatch(&d->pi, DISPATCH_NEW_PAGE, full_path);
   }
   return true;
 }
@@ -63,18 +83,20 @@ int file_compare(const void *a, const void *b) {
 
 bool gen_items_from_dir(const char *wd, menu_item_array *buf) {
   if (wd == NULL) return false;
-  if (dir != NULL) {
-    closedir(dir);
-    dir = NULL;
+  if (dir_t.dir != NULL) {
+    closedir(dir_t.dir);
+    dir_t.dir = NULL;
   }
   struct dirent *dp = NULL;
-  if ((dir = opendir(wd)) == NULL) {
+  if ((dir_t.dir = opendir(wd)) == NULL) {
     fprintf(stderr, "failed to open directory %s\n", wd);
     return false;
   }
   do {
-    if ((dp = readdir(dir)) != NULL) {
+    if ((dp = readdir(dir_t.dir)) != NULL) {
       struct menu_item mi;
+      // since we just reuse the dir's char pointer
+      // we do not free the dir immediately
       mi.name = dp->d_name;
       mi.selected = file_selected;
       mi.ctx = dp->d_name;
@@ -82,12 +104,16 @@ bool gen_items_from_dir(const char *wd, menu_item_array *buf) {
     }
   } while (dp != NULL);
   qsort(buffer.menu_item_data, buffer.len, sizeof(struct menu_item), file_compare);
-  //closedir(dir);
   return true;
 }
 
 bool action(struct plugin_interface *d) {
-  if (!gen_items_from_dir(cwd, &buffer)) {
+  // reset cur_dir to base path
+  if (dir_t.base != dir_t.cur_dir) {
+    free(dir_t.cur_dir);
+    dir_t.cur_dir = dir_t.base;
+  }
+  if (!gen_items_from_dir(dir_t.base, &buffer)) {
     return false;
   }
   d->dispatch(d, DISPATCH_MENU, &buffer);
@@ -99,11 +125,11 @@ void get_display_prompt(const char **out) {
 }
 
 bool cleanup(struct plugin_interface* pi) {
-  if (dir != NULL) {
-    closedir(dir);
-    dir = NULL;
+  if (dir_t.dir != NULL) {
+    closedir(dir_t.dir);
+    dir_t.dir = NULL;
   }
-  free(cwd);
+  free(dir_t.base);
   if (buffer.menu_item_data != NULL) {
     free_menu_item_array(&buffer);
   }
