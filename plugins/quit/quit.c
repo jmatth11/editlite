6545e9d91f61759321db619d6e3b10ff465168eb
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:53 -0600

    added compile flags for lsp stuff

--- plugins/quit/quit.c
+++ plugins/quit/quit.c
@@ -4,6 +4,7 @@ const char *prompt = "quit application";
 
 bool action(struct plugin_interface *d) {
   d->dispatch(d, DISPATCH_EXIT, NULL);
+  return true;
 }
 void get_display_prompt(const char **out) {
   *out = prompt;
