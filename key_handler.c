83a7f69498f279bb56a9cf498a20f6dfd9f39b09
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:11:54 -0600

    cleaned up cursor movement

--- key_handler.c
+++ key_handler.c
@@ -4,24 +4,24 @@
 
 void handle_row_scroll(struct display *d, struct display_dim dim) {
   struct page *cur_page = &d->page_mgr.pages.page_data[d->cur_buf];
-  if (d->cursor.dim.row >= cur_page->lines.len) {
-    d->cursor.dim.row = cur_page->lines.len - 1;
-  } else if (d->cursor.dim.row >= cur_page->row_offset + dim.row) {
+  if (d->cursor.pos.row >= cur_page->lines.len) {
+    d->cursor.pos.row = cur_page->lines.len - 1;
+  } else if (d->cursor.pos.row >= cur_page->row_offset + dim.row) {
     cur_page->row_offset++;
-  } else if (d->cursor.dim.row < cur_page->row_offset) {
-    cur_page->row_offset = d->cursor.dim.row;
+  } else if (d->cursor.pos.row < cur_page->row_offset) {
+    cur_page->row_offset = d->cursor.pos.row;
   }
 }
 
 void handle_col_scroll(struct display *d, struct display_dim dim) {
   struct page *cur_page = &d->page_mgr.pages.page_data[d->cur_buf];
-  struct line *cur_line = &cur_page->lines.line_data[d->cursor.dim.row];
-  if (d->cursor.dim.col >= cur_line->chars.len) {
-    d->cursor.dim.col = cur_line->chars.len - 1;
-  } else if (d->cursor.dim.col >= cur_page->col_offset + dim.col) {
+  struct line *cur_line = &cur_page->lines.line_data[d->cursor.pos.row];
+  if (d->cursor.pos.col >= cur_line->chars.len) {
+    d->cursor.pos.col = cur_line->chars.len - 1;
+  } else if (d->cursor.pos.col >= cur_page->col_offset + dim.col) {
     cur_page->col_offset++;
-  } else if (d->cursor.dim.col < cur_page->col_offset) {
-    cur_page->col_offset = d->cursor.dim.col;
+  } else if (d->cursor.pos.col < cur_page->col_offset) {
+    cur_page->col_offset = d->cursor.pos.col;
   }
 }
 
@@ -30,19 +30,20 @@ void keydown_handler(struct display *d, struct win *w, SDL_Event *e) {
   get_page_dim(d, w, &dims);
   switch (e->key.keysym.sym) {
     case SDLK_j:
-      d->cursor.dim.row++;
+      d->cursor.pos.row++;
       handle_row_scroll(d, dims);
       break;
     case SDLK_k:
-      d->cursor.dim.row = MAX(0, d->cursor.dim.row - 1);
+      d->cursor.pos.row = MAX(0, d->cursor.pos.row - 1);
       handle_row_scroll(d, dims);
       break;
     case SDLK_l:
-      d->cursor.dim.col++;
+      d->cursor.pos.col++;
       handle_col_scroll(d, dims);
       break;
     case SDLK_h:
-      d->cursor.dim.col = MAX(0, d->cursor.dim.col - 1);
+      if (d->cursor.pos.col != d->cursor.screen_pos.col) d->cursor.pos.col = d->cursor.screen_pos.col;
+      d->cursor.pos.col = MAX(0, d->cursor.pos.col - 1);
       handle_col_scroll(d, dims);
       break;
   }
